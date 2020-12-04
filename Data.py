763155784801f0a9333e094c7612c519747af627
AmirCM <amirbehzadfar.h@gmail.com>
2020-12-04 18:50:59 +0330

    Compile data

--- Data.py
+++ Data.py
@@ -3,24 +3,72 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import mplfinance as mpf
 import matplotlib.dates as mdates
-import pandas as pd
+import pandas_datareader.data as web
 import requests
 import pickle
 import bs4 as bs
+import os
 
 
 def save_sp500_tickers():
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
     table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker)
+
+    print(len(tickers), tickers)
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+    return tickers
 
 
-style.use('ggplot')
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
 
-df = pd.read_csv('TSLA.csv', parse_dates=True, index_col=0)
+    start = dt.datetime(2010, 1, 1)
+    end = dt.datetime.now()
+    for ticker in tickers[:10]:
+        # just in case your connection breaks, we'd like to save our progress!
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'morningstar', start, end)
+            df.reset_index(inplace=True)
+            df.set_index("Date", inplace=True)
+            df = df.drop("Symbol", axis=1)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
 
-df_ohlc = df['Adj Close'].resample('10D').ohlc()
-df_volume = df['Volume'].resample('10D').sum()
-df_ohlc['volume'] = df_volume
 
-mpf.plot(df_ohlc, type='candle', title='TSLA STOCK', volume=True, style='yahoo')
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close': ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+compile_data()
